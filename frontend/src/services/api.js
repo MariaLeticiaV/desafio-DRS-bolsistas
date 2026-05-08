40e8b16403e82770f4c6df0720bc78cc784c5642
MariaLeticiaV <mlvo@ic.ufal.br>
2026-05-07 21:04:29 -0300

    corrigindo consumo api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,37 +1,55 @@
 const BASE_URL = "https://pipeapi-backend.onrender.com";
 
-export async function fetchSummary(temperature, pressure) {
-  const res = await fetch(`${BASE_URL}/api/summary`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ temperature, pressure }),
-  });
-
-  const data = await res.json();
-
-  if (!res.ok) throw new Error(data.error);
-
-  return data;
-}
-
-export async function fetchChart(temp, pressure) {
+export async function fetchSummary(
+  temperature,
+  pressure
+) {
   const res = await fetch(
-    `${BASE_URL}/api/chart?temperature=${temp}&pressure=${pressure}`
+    `${BASE_URL}/api/summary`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        temperature: Number(temperature),
+        pressure: Number(pressure),
+      }),
+    }
   );
 
   const data = await res.json();
 
-  if (!res.ok) throw new Error(data.error);
+  if (!res.ok) {
+    throw new Error(data.error);
+  }
 
-  return data.data;
+  return data;
 }
 
-export async function fetchHistory() {
-  const res = await fetch(`${BASE_URL}/api/history`);
+export async function fetchChart(
+  temperature,
+  pressure
+) {
+  const res = await fetch(
+    `${BASE_URL}/api/chart`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        temperature: Number(temperature),
+        pressure: Number(pressure),
+      }),
+    }
+  );
 
   const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(data.error);
+  }
 
   return data.data;
 }
