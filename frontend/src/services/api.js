57ff98f574ba8964f335de2ad8bf83e6d30b0622
MariaLeticiaV <mlvo@ic.ufal.br>
2026-05-07 21:10:21 -0300

    corrigindo consumo da api render

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,54 +1,38 @@
 const BASE_URL = "https://pipeapi-backend.onrender.com";
 
-export async function fetchSummary(
-  temperature,
-  pressure
-) {
-  const res = await fetch(
-    `${BASE_URL}/api/summary`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        temperature: Number(temperature),
-        pressure: Number(pressure),
-      }),
-    }
-  );
+export async function fetchSummary(temperature, pressure) {
+  const res = await fetch(`${BASE_URL}/api/summary`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      temperature: Number(temperature),
+      pressure: Number(pressure),
+    }),
+  });
 
   const data = await res.json();
 
   if (!res.ok) {
-    throw new Error(data.error);
+    throw new Error(data.error || "Erro ao buscar summary");
   }
 
   return data;
 }
 
-export async function fetchChart(
-  temperature,
-  pressure
-) {
+export async function fetchChart(temperature, pressure) {
   const res = await fetch(
-    `${BASE_URL}/api/chart`,
+    `${BASE_URL}/api/chart?temperature=${temperature}&pressure=${pressure}`,
     {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        temperature: Number(temperature),
-        pressure: Number(pressure),
-      }),
     }
   );
 
   const data = await res.json();
 
   if (!res.ok) {
-    throw new Error(data.error);
+    throw new Error(data.error || "Erro ao buscar gráfico");
   }
 
   return data.data;
